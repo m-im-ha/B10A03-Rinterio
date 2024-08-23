43efc56cf34e126f221793cd67ae83b8b10fa806
imran <sherlockholmesad221@gmail.com>
2024-08-23 13:04:44 +0600

    edit work process part

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       'color-secondary': '#494949',
       'color-btn-primary': '#ABEF5F',
       'color-btn-secondary': 'rgba(171,239,95,.5)',
+      'color-card-one': '#E7C1D3',
+      'color-card-two': '#EFDA6E',
+      'color-card-three': '#A4DAC3',
+      'color-card-four': '#77AAEA',
 
     },},
     
